e30d8e14db78a8894354d9399de37dc26addba87
Lorenzo <lorezanchetta@gmail.com>
2022-08-03 16:15:23 +0200

    some

--- liv/script/video.js
+++ liv/script/video.js
@@ -48,36 +48,3 @@ function playAudio(){
     introVideo.muted = !introVideo.muted;
 
 }
-
-gsap.registerPlugin(ScrollTrigger);
-
-const bgVvideo = document.querySelector("#bgVideo");
-
-bgVideo.pause();
-bgVideo.currentTime = 0;
-
-let sections = gsap.utils.toArray(".step");
-sections.forEach((step, i) => {
-
-    ScrollTrigger.create({
-        trigger: step,
-        start: "bottom bottom",
-        end: "+=1000",
-        pin: true,
-        anticipatePin: 1,
-    });
-
-    gsap.fromTo(bgVideo, { currentTime: 3 * i }, {
-        scrollTrigger: {
-            trigger: step,
-            scrub: 2,
-            start: "top bottom",
-            end: "bottom bottom",
-        },
-        currentTime: 3 * (i + 1),
-        duration: 3,
-        ease: "none",
-
-    });
-
-});
