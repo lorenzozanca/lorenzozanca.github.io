dd27668bc10652d55ddd02109183ed55de00ca21
Lorenzo <lorezanchetta@gmail.com>
2022-08-16 15:26:36 +0200

    today

--- liv/script/narration.js
+++ liv/script/narration.js
@@ -6,7 +6,7 @@
     var section1 = document.getElementById("section1").getBoundingClientRect();
 
         if (0 > section1.top) {
-            description.innerHTML = "Imagine a future where your digital footprint wherever you are is turned into health data";
+            description.innerHTML = "Imagine the future of health";
             description.style.opacity = "1";
 
             var section2 = document.getElementById("section2").getBoundingClientRect();
@@ -15,7 +15,7 @@
 
                 var section3 = document.getElementById("section3").getBoundingClientRect();
                 if (0 > section3.top) {
-                    description.innerHTML = "Imagine, if we could used it proactively for preventive healthcare";
+                    description.innerHTML = "where your digital footprint – wherever you are";
                     description.style.opacity = "1";
 
                     var section4 = document.getElementById("section4").getBoundingClientRect();
@@ -24,14 +24,14 @@
 
                         var section5 = document.getElementById("section5").getBoundingClientRect();
                         if (0 > section5.top) {
-                            description.innerHTML = "Imagine if we could analyze the entire body";
-                            description.style.opacity = "1";
+                            description.innerHTML = "";
+                            description.style.opacity = "0";
 
-                            var section6 = document.getElementById("section6").getBoundingClientRect();
-                            if (0 > section6.top) {
-                                description.innerHTML = "";
-                                description.style.opacity = "0";
-                            }
+                            // var section6 = document.getElementById("section6").getBoundingClientRect();
+                            // if (0 > section6.top) {
+                            //     description.innerHTML = "";
+                            //     description.style.opacity = "0";
+                            // }
                         }
                     }
                 }
