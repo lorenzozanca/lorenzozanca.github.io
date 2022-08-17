ec60695cf0233a5cb9045f3f72e82f42ea564286
Lorenzo <lorezanchetta@gmail.com>
2022-08-17 11:50:46 +0200

    mobilefix

--- liv/old/narration.js
+++ liv/old/narration.js
@@ -42,16 +42,3 @@
     // }
 
     // window.addEventListener("scroll", narration);
-
-const containers = gsap.utils.toArray(".container");
-containers.forEach((container) => {
-  gsap.to(container, {
-    autoAlpha: 1,
-    ease: "power1.in",
-    scrollTrigger: {
-      trigger: container,
-      pin: true,
-      scrub: true
-    }
-  });
-});
